ad1c5bd4a7a6770e07df5d570a49fbfc876af777
vijay-kumar-yadav <vky405021@gmail.com>
2021-07-31 17:37:04 +0530

    Created different switch  pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import './styles/main.scss';
 import 'rsuite/dist/styles/rsuite-default.css';
+import { Route, Switch } from 'react-router';
+import Home from './Pages/Home';
+import Category from './Pages/Category';
+import Checkout from './Pages/Checkout';
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/Category">
+          <Category />
+        </Route>
+        <Route exact path="/Checkout">
+          <Checkout />
+        </Route>
+        <Route>
+          <div>Not found</div>
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
